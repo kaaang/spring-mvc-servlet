753c1ab4a4c19766c8d3603f3dac1a84a391356f
kaaang <kaaang@kakao.com>
2022-08-28 00:14:52 +0900

    05

--- src/main/java/hello/servlet/basic/request/RequestParamServlet.java
+++ src/main/java/hello/servlet/basic/request/RequestParamServlet.java
@@ -24,11 +24,14 @@ public class RequestParamServlet extends HttpServlet {
         System.out.println("RequestParamServlet.service");
         System.out.println();
 
+
         System.out.println("[전체 파라미터 조회] - start");
         request.getParameterNames().asIterator()
                         .forEachRemaining(paramName -> System.out.println(paramName + "=" + request.getParameter(paramName)));
         System.out.println();
         System.out.println("[전체 파라미터 조회] - end");
+
+        System.out.println();
 
         System.out.println("[단일 파라미터 조회]");
         String username = request.getParameter("username");
